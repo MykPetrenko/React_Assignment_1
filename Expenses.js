afb27c1e2a22da36ef71e4c36b45d61877820a26
MykPetrenko <125318008+MykPetrenko@users.noreply.github.com>
2023-07-20 15:29:06 +0300

    Update Expenses.js

--- Expenses.js
+++ Expenses.js
@@ -1,14 +1,31 @@
-import './ExpenseItem.css'
-import ExpenseDate from "./ExpenseDate";
-const ExpenseItem = ({date, title, amount}) => {
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+
+const Expenses = (props) => {
+  const expenses = props.expenses;
   return (
-    <div className={'expense-item'}>
-      <ExpenseDate date={date}/>
-      <div className={'expense-item__description'}>
-        <h2>{title}</h2>
-        <div className={'expense-item__price'}>${amount}</div>
-      </div>
+    <div className={'expenses'}>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
-export default ExpenseItem;
+export default Expenses;
